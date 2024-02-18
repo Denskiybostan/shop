2f81b9ec520679382e52df6b1caa9e163e43c0fb
denis <deniz246@mail.ru>
2024-02-18 20:53:38 +0300

    1

--- src/main/java/sky/pro/demo/Services/ShopService.java
+++ src/main/java/sky/pro/demo/Services/ShopService.java
@@ -1,9 +1,12 @@
 package sky.pro.demo.Services;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 import sky.pro.demo.ShoppingCart;
 
 import java.util.List;
-
+@Component
+@SessionScope
 public class ShopService {
     private final ShoppingCart shoppingCart = new ShoppingCart();
     public void add (Integer... ids) {
